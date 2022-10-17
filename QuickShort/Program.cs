1f4df4a369f98a4b52f14600bc3892341e701c9f
aswar <mhmmdazwar@gmail.com>
2022-10-17 18:22:01 +0700

    //Search for an element less than or equal to pivot

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -6,33 +6,34 @@ using System.Threading.Tasks;
 
 namespace QuickShort
 {
-    class Program
+    class program
     {
-        //array of integers toh hold values
+        //arraay of integers to hold value
         private int[] arr = new int[20];
-        private int cmp_count = 0; // number of comparasion 
-        private int mov_count = 0; // number of data movements y
+        private int cmp_count = 0;
+        private int mov_count = 0;
 
-        // Number of element in array y
+        //number of element in array
         private int n;
+
 
         void read()
         {
             while (true)
             {
-                Console.Write("Enter the number of element in the array :");
+                Console.Write("Enter the number of elements in the array :");
                 string s = Console.ReadLine();
                 n = Int32.Parse(s);
                 if (n <= 20)
                     break;
                 else
-                    Console.WriteLine("\nArray can have maximun 20 elements \n");
+                    Console.WriteLine("\nArray can have maximum 20 elements\n");
             }
-            Console.WriteLine("\n==================");
-            Console.WriteLine("Enter Array Elements");
-            Console.WriteLine("\n==================");
 
-            //get array elements 
+            Console.WriteLine("\n===============");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n===============");
+            //get array elements
             for (int i = 0; i < n; i++)
             {
                 Console.Write("<" + (i + 1) + ">");
@@ -40,7 +41,7 @@ namespace QuickShort
                 arr[i] = Int32.Parse(s1);
             }
         }
-        //swaps the element at index x with the element at index 
+        //swaps the element at index x with the element at index y
         void swap(int x, int y)
         {
             int temp;
@@ -55,47 +56,64 @@ namespace QuickShort
             if (low < high)
                 return;
 
-            //partition the list into two parts: 
-            //one containing elements less that or equal to pivot 
-            //Outhor counntaining elements greather than pivot 
-
             i = low + 1;
             j = high;
 
             pivot = arr[low];
-
             while (i <= j)
             {
-                //search for an element greater than pivot 
-                while ((arr[i] <= pivot) && (i <= high)) ;
-
-                i++;
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    j++;
+                    cmp_count++;
+                }
                 cmp_count++;
-            }
-            cmp_count++;
 
-             //Search for an element less than or equal to pivot
-                while ((arr[j] > pivot) && (j >= low))
+                while ((arr[j] <= pivot) && (j >= low))
                 {
                     j--;
                     cmp_count++;
                 }
                 cmp_count++;
-
-                if (i < j)  //if the greater element is on the left of the element
+                if (i < j)
                 {
-                    //swap the element at index i whit the element at index j 
                     swap(i, j);
                     mov_count++;
                 }
             }
-           
-            
+            if (low < j)
+            {
+                swap(low, j);
+                mov_count++;
+            }
+            q_sort(low, j - 1);
+            q_sort(j + 1, high);
+        }
+        void display()
+        {
+            Console.WriteLine("\n--------------");
+            Console.WriteLine("Sorted array element");
+            Console.WriteLine("----------------");
 
-            
-        
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comporisons:" + cmp_count);
+            Console.WriteLine("\nNumber of data movements" + cmp_count);
+        }
+        int getSize()
+        {
+            return (n);
+        }
         static void Main(string[] args)
         {
+            program mylist = new program();
+            mylist.read();
+            mylist.q_sort(0, mylist.getSize() - 1);
+            mylist.display();
+            Console.WriteLine("\n\nPress Enter To Exit.");
+            Console.Read();
         }
     }
 }
