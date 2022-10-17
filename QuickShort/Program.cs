7967c4429dec8db524a17e06295bcd925c863361
aswar <mhmmdazwar@gmail.com>
2022-10-17 17:45:50 +0700

    //search for an element greater than pivot

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -66,7 +66,7 @@ namespace QuickShort
 
             while (i <= j)
             {
-                //search for an element greater than pivot
+                //search for an element greater than pivot 
                 while ((arr[i] <= pivot) && (i <= high)) ;
 
                 i++;
